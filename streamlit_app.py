e75578a037bc2342c7f4cb10699f7d0cbe7d86d3
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-06-20 01:52:13 +0000

    added contact form template

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,13 @@ with tab4:
     st.write("WIP")
 
 with tab5:
-    st.write("EMAIL: fcat.repository@gmail.com")
+    st.write("submitting this form doesn't do anything yet FYI")
+    with st.form("form2", clear_on_submit=True):
+        name = st.text_input("Enter name")
+        email = st.text_input("Enter email")
+        message = st.text_area("Enter message")
+        submit = st.form_submit_button("Submit")
+    st.write("Or send email to: fcat.repository@gmail.com (can add file attachments this way)")
 
 with tab6:
     st.write("Subscription benefits:")
