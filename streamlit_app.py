0a190dd20c222dd587b30e8c30fba3e9ef18385d
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-06-20 01:36:09 +0000

    example of pricing tab

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,22 @@ with tab5:
     st.write("EMAIL: fcat.repository@gmail.com")
 
 with tab6:
-    st.write("Currently this Repository is implemented as a demo. Consider adding to the repository through the contribute tab if you found this Repository useful. ")
+    st.write("Subscription benefits:")
+    lst = ['access to the entire repository data', 
+           'access to insights charts', 
+           'tools to generate professional figures from your data']
+    s = ''
+    for i in lst:
+        s += "- " + i + "\n"
+    st.markdown(s)
+    st.write("Pricing")
+    lst = ["$xx/month for individual access", 
+           "email about discounts for group and organizational level access",
+           "free access for x months for contributing to repository"]
+    s = ''
+    for i in lst:
+        s += "- " + i + "\n"
+    st.markdown(s)
 
 
 # if keycloak.authenticated:
