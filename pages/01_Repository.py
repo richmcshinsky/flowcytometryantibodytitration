c17144e2b0c50c43367f8f1c92040a39d58286b8
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-07-03 02:59:53 +0000

    update

--- pages/01_Repository.py
+++ pages/01_Repository.py
@@ -33,7 +33,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="testing", ttl="30m")
 
 # st.write("If you aren't able to find your target antigen, try an alternative name! Or add alternate names to your filter for more data.")
-search_target = st.text_input("If you aren't able to find your target antigen, try an alternative name! Or add alternate names to your filter for more data.", "For example: cd279")
+search_target = st.text_input("If you aren't able to find your target antigen, try an alternative name! Or add alternate names to your filter for more data.")
 if search_target:
     df_terms = pd.read_excel("data/CD alternative names.xlsx", names=["cd", "alternate"])
     st.dataframe(df_terms[df_terms["cd"].str.contains(search_target, case=False)])
