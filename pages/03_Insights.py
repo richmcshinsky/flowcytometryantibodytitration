6ba5e8086a2fb9c9dfe123759c3e86eb1eafd6b1
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-07-20 03:52:08 +0000

    t

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -49,10 +49,10 @@ st.plotly_chart(fig, use_container_width=True)
 
 st.write("Price comparison between suppliers")
 res_p = res[["Supplier", "supplier price"]].dropna()
+res_p = res_p[res_p["supplier price"] != "nan"]
 res_p["supplier price"] = [float(x.replace("€", "")) * 1.29 if "€" in x else x for x in res_p["supplier price"]]
 fig = px.box(res_p, x="Supplier", y="supplier price")
 st.plotly_chart(fig, use_container_width=True)
-st.write(res_p[res_p["Supplier"] == "BC"]["supplier price"].values[0].isna())
 fig = px.scatter(res_p, x="Supplier", y="supplier price")
 st.plotly_chart(fig, use_container_width=True)
 
