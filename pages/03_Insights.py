0562becc853cb37c34d5e9dd26405f1d1c338c95
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-07-20 03:43:15 +0000

    t

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -48,11 +48,11 @@ fig = px.bar(res["Antigen"].value_counts(normalize=True)[:20])
 st.plotly_chart(fig, use_container_width=True)
 
 st.write("Price comparison between suppliers")
-res_p = res["Supplier", "supplier price"].dropna()
-res_p["supplier price"] == [float(x.replace("€", "")) * 1.29 if "€" in x else x for x in res_p["supplier price"]]
+res_p = res[["Supplier", "supplier price"]].dropna()
+res_p["supplier price clean"] == [float(x.replace("€", "")) * 1.29 if "€" in x else x for x in res_p["supplier price"]]
 st.write(res_p)
 st.write("Price comparison between suppliers")
-fig = px.box(res_p, x="Supplier", y="supplier price")
+fig = px.box(res_p, x="Supplier", y="supplier price clean")
 st.plotly_chart(fig, use_container_width=True)
 
 add_auth(required=True)
