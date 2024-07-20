23a7b110d318b6a49c9c9e8a346db4109d3ebcd5
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-07-20 15:50:55 +0000

    t

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -48,7 +48,7 @@ fig = px.bar(res["Antigen"].value_counts(normalize=True)[:20])
 st.plotly_chart(fig, use_container_width=True)
 
 st.write("Price comparison between suppliers")
-res_p = res[["Supplier", "supplier price"]].dropna()
+res_p = res[["Source", "Antigen", "Supplier", "supplier price"]].dropna().drop_duplicates()
 res_p = res_p[res_p["supplier price"] != "nan"]
 res_p["supplier price"] = [float(x.replace("€", "")) * 1.29 if "€" in x else x for x in res_p["supplier price"]]
 fig = px.box(res_p, x="Supplier", y="supplier price")
