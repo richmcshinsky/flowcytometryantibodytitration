ecadd5667c604b1868732acfdc3c59b3be62b90a
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-07-21 03:07:33 +0000

    t

--- pages/06_Guided.py
+++ pages/06_Guided.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-import streamlit_pandas as sp
+from streamlit_dynamic_filters import DynamicFilters
 
 st.set_page_config(page_title='Flow Cytometry Antibody Titration Repository', layout="wide")
 
@@ -17,12 +17,55 @@ with st.sidebar:
 st.markdown("<h1 style='text-align: center; color: black;'>Metrdy</h1>", unsafe_allow_html=True)
 st.divider()
 
+@st.cache_data(show_spinner=False)
+def load_data():
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read(worksheet="testing", ttl="30m")
+    df["Supplier"] = df["Supplier"].replace(["Biolegend", "BL"], "BioLegend")
+    df["Supplier"] = df["Supplier"].replace(["BD Horizon", "BD pharmingen", "BD Biosciences", "BD Pharm",
+                                                    "BD Pharmingen", "BD Special order reagent", "BD OptiBuild",
+                                                    "BD", "BD custom antibody"], "BD Bioscience")
+    df["Supplier"] = df["Supplier"].replace(["eBiosciences", "ebioscience", "eBioscinece"], "eBioscience")
+    df["Supplier"] = df["Supplier"].replace(["Thermo Fisher Scientific", "ThermoFisher", "Thermo",
+                                                    "ThermoFisher Scientific", "Thermofisher"], "Thermo Fisher")
+    df["Supplier"] = df["Supplier"].replace(["Miltenyi Biotec", "BL"], "Miltenyi")
+    df = normalize_antigens(df)
+    return df[columns]
+
+def normalize_antigens(df):
+    df_terms = pd.read_excel("data/CD alternative names.xlsx", names=["cd", "alternate"]).fillna("NULL")
+    df_terms["alternate"] = ["NULL" if x == "-" else x for x in df_terms["alternate"]]
+    rename = []
+    for a in df["Antigen"].fillna(""):
+        new_a = a
+        for cd, alts in zip(df_terms["cd"], df_terms["alternate"]):
+            for alt in alts.split(","):
+                if alt in a:
+                    new_a = cd
+                    break
+            if new_a != a:
+                break
+        rename.append(new_a)
+    df["Antigen"] = rename
+    return df
+
 st.write("What protocol do you plan to run?")
+df = load_data()
+df_filtered = DynamicFilters(df.astype(str).fillna(""), filters=["Conjugate Type"])
+df_filtered.display_filters()
+
 col1, col2 = st.columns(2, gap="small")
 with col1:
     if st.button(label="Flow Cytometry", use_container_width=True):
         protocol = "flow"
+        # conjugate type column filter to flouresent
 with col2:
     if st.button(label="Mass Cytometry", use_container_width=True):
         protocol = "mass"
 
+df_filtered = DynamicFilters(df.astype(str).fillna(""), filters=["Antigen"])
+df_filtered.display_filters()
+# select antigen 
+# select conjugate or clone
+
+# show graph of cost/sample and graph of separation index by other (fluorophore or clone)​
