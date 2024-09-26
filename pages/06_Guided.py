c0e85028ed1bcb36caa59cd3f414fbc0c7ee1984
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-09-26 20:39:50 +0000

    t

--- pages/06_Guided.py
+++ pages/06_Guided.py
@@ -52,7 +52,11 @@ def normalize_antigens(df):
     return df
 
 
-def step_1():
+
+if "step" not in st.session_state:
+    st.session_state["step"] = "Step 1"
+
+elif st.session_state["step"] == "Step 1":
     st.write("Step 1: What protocol do you plan to run?")
     col1, col2 = st.columns(2, gap="small")
     with col1:
@@ -64,113 +68,61 @@ def step_1():
             st.session_state["con_type"] = "Metal"
             st.session_state["step"] = "Step 2"
 
-def step_2():
+elif st.session_state["step"] == "Step 2":
     st.write("Step 2: Select target antigen")
     st.session_state["df"] = load_data()
     ants = st.session_state["df"][st.session_state["df"]["Conjugate Type"] == st.session_state["con_type"]]['Antigen'].drop_duplicates()
     ants_choice = st.selectbox("Select your target antigen", options=ants, index=None)
-    st.session_state["ants_choice"] = ants_choice
-
-
-if "step" not in st.session_state:
-    st.session_state["step"] = "Step 1"
-elif st.session_state["step"] == "Step 2":
-    st.session_state["ants_choice"] = step_2()
-# elif st.session_state["step"] == "Step 3":
-#     st.session_state["type"] = step_3()
-# elif st.session_state["step"] == "Step 4":
-#     st.session_state["choice"] = step_4(st.session_state["con_type"], st.session_state["ants_choice"], st.session_state["type"])
-
-
-
-
-
-
-
-##########################################
-"""def step_1():
-    st.write("Step 1: What protocol do you plan to run?")
-    con_type = None
-    col1, col2 = st.columns(2, gap="small")
-    with col1:
-        if st.button(label="Flow Cytometry", use_container_width=True):
-            con_type = "Fluorescent"
-            # move to next step on new page or something?
-    with col2:
-        if st.button(label="Mass Cytometry", use_container_width=True):
-            con_type = "Metal"
-            # move to next step on new page or something?
-    
-    return con_type
-
-def step_2(con_type):
-    st.write("Step 2: Select target antigen")
-    ants = df[df["Conjugate Type"] == con_type]['Antigen'].drop_duplicates()
-    ants_choice = st.selectbox("Select your target antigen", options=ants, index=None)
-    
-    return ants_choice
-
-def step_3():
-    st.write("Step 3: Fluorophore or Clone?")
-    choice = None
-    col1, col2 = st.columns(2, gap="small")
-    with col1:
-        if st.button(label="Fluorophore", use_container_width=True):
-            choice = "Conjugate"
-            # move to next step on new page or something?
-    with col2:
-        if st.button(label="Clone", use_container_width=True):
-            choice = "Clone"
-    
-    return choice
-
-def step_4(con_type, ants_choice, choice):
-    res = None
-    if choice == "Conjugate":
-        st.write("Step 4: Select target Fluorophore")
-        cons = df[(df["Conjugate Type"] == con_type) & (df["Antigen"] == ants_choice)]['Conjugate'].drop_duplicates()
-        res = st.selectbox("Select your target fluorophore", options=cons, index=None)
-        return res
-    elif choice == "Clone":
-        st.write("Step 4: Select target Clone")
-        clos = df[(df["Conjugate Type"] == con_type) & (df["Antigen"] == ants_choice)]['Clone'].drop_duplicates()
-        res = st.selectbox("Select your target clone", options=clos, index=None)
-        return res
-    
-
-df = load_data()
-
-st.session_state["step"] = st.selectbox("Step", ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5"])
-
-if st.session_state["step"] == "Step 1":
-    st.session_state["con_type"] = step_1()   
-
-elif st.session_state["step"] == "Step 2":
-    st.session_state["ants_choice"] = step_2(st.session_state["con_type"])
+    if st.button(label="Next", use_container_width=True):
+        st.session_state["ants_choice"] = ants_choice
+        st.session_state["step"] = "Step 3"
 
 elif st.session_state["step"] == "Step 3":
-    st.session_state["type"] = step_3()
-
+    st.write("Step 3: Fluorophore (Conjugate) or Clone?")
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        if st.button(label="Fluorophore (Conjugate)", use_container_width=True):
+            st.session_state["choice"] = "Conjugate"
+            st.session_state["step"] = "Step 4"
+    with col2:
+        if st.button(label="Clone", use_container_width=True):
+            st.session_state["choice"] = "Clone"
+            st.session_state["step"] = "Step 4"
+    
 elif st.session_state["step"] == "Step 4":
-    st.session_state["choice"] = step_4(st.session_state["con_type"], st.session_state["ants_choice"], st.session_state["type"])
+    if st.session_state["choice"] == "Conjugate":
+        st.write("Step 4: Select target Fluorophore")
+        cons = st.session_state["df"][(st.session_state["df"]["Conjugate Type"] == st.session_state["con_type"]) & (st.session_state["df"]["Antigen"] == st.session_state["ants_choice"])]['Conjugate'].drop_duplicates()
+        res = st.selectbox("Select your target fluorophore", options=cons, index=None)
+        if st.button(label="Next", use_container_width=True):
+            st.session_state["target"] = res
+            st.session_state["step"] = "Step 5"
+    elif st.session_state["choice"] == "Clone":
+        st.write("Step 4: Select target Clone")
+        clos = st.session_state["df"][(st.session_state["df"]["Conjugate Type"] == st.session_state["con_type"]) & (st.session_state["df"]["Antigen"] == st.session_state["ants_choice"])]['Clone'].drop_duplicates()
+        res = st.selectbox("Select your target clone", options=clos, index=None)
+        if st.button(label="Next", use_container_width=True):
+            st.session_state["target"] = res
+            st.session_state["step"] = "Step 5"
 
 elif st.session_state["step"] == "Step 5":
-    if st.session_state["type"] == "Conjugate":
+    df = st.session_state["df"]
+    if st.session_state["choice"] == "Conjugate":
         df_g = df[(df["Conjugate Type"] == st.session_state["con_type"]) & 
                     (df["Antigen"] == st.session_state["ants_choice"]) & 
-                    (df["Conjugate"] == st.session_state["choice"])]
-    elif st.session_state["type"] == "Clone":
+                    (df["Conjugate"] == st.session_state["target"])]
+    elif st.session_state["choice"] == "Clone":
         df_g = df[(df["Conjugate Type"] == st.session_state["con_type"]) & 
                     (df["Antigen"] == st.session_state["ants_choice"]) & 
-                    (df["Clone"] == st.session_state["choice"])]
+                    (df["Clone"] == st.session_state["target"])]
     
     # show graph of cost/sample and graph of separation index by other (fluorophore or clone)
-    st.write(Price comparison between suppliers. Note that the supplier information is web scraped, so 
-             there are cases where the size or price may be incorrect. Please see supplier link
-             for current and correct information! Note that prices in pounds have been converted to 
-             dollars by multiplying by 1.29.)
+    st.write("""Price comparison between suppliers. Note that the supplier information is web scraped, so 
+            there are cases where the size or price may be incorrect. Please see supplier link
+            for current and correct information! Note that prices in pounds have been converted to 
+            dollars by multiplying by 1.29.""")
     res_p = df_g[["Source", "supplier link", "Antigen", "Conjugate Type", "Conjugate", "Clone", "Supplier", 
-                  "supplier price", "supplier size"]].dropna().drop_duplicates()
+                "supplier price", "supplier size"]].dropna().drop_duplicates()
     res_p = res_p[res_p["supplier price"] != "nan"]
     res_p["supplier price"] = [float(x.replace("€", "")) * 1.29 if "€" in x else float(x.replace("$", "")) for x in res_p["supplier price"]]
     res_p['supplier size'] = res_p['supplier size'].str.extract('(\d+)', expand=False).astype(int)
@@ -178,9 +130,10 @@ elif st.session_state["step"] == "Step 5":
 
     fig = px.strip(res_p, x="Supplier", y="supplier price/size")
     st.plotly_chart(fig, use_container_width=True)
-
-    # st.write(res_p)
     st.dataframe(data=res_p, use_container_width=True, 
-                 column_config={"Source": st.column_config.LinkColumn(display_text="Source"),
+                column_config={"Source": st.column_config.LinkColumn(display_text="Source"),
                                 "supplier link": st.column_config.LinkColumn(display_text="Supplier Link")})
-"""
+
+
+
+
