e691d3d03f04624db9434f8205c8cd6fa683bec8
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-09-27 08:26:52 +0000

    t

--- pages/06_Guided.py
+++ pages/06_Guided.py
@@ -168,8 +168,8 @@ elif st.session_state["step"] == "Step 5":
                 "price/test at optimal uL", "reorder frequency at 10 tests/week (years)"]].dropna().drop_duplicates()
     res_p = res_p[res_p["supplier price"] != "nan"]
     res_p["supplier price"] = [float(x.replace("€", "")) * 1.29 if "€" in x else float(x.replace("$", "")) for x in res_p["supplier price"]]
-    res_p['supplier size'] = res_p['supplier size'].str.extract('(\d+)', expand=False).astype(int)
-    res_p["supplier price/size"] = res_p["supplier price"]/res_p["supplier size"]# supplier size
+    res_p["supplier # tests"] = res_p["supplier # tests"].str.extract('(\d+)', expand=False).astype(int)
+    res_p["supplier price/size"] = res_p["supplier price"]/res_p["supplier # tests"]# supplier size
 
     fig = px.strip(res_p, x="Supplier", y="supplier price/size")
     st.plotly_chart(fig, use_container_width=True)
