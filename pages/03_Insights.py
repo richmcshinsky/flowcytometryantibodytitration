1ee00ef1ae72abe40244c3b0b91335ab1cb4e0a0
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-09-27 16:28:11 +0000

    t

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -88,9 +88,32 @@ st.write("reorder frequency at 10 tests/week (years) comparison between supplier
 fig = px.strip(res_p, x="Supplier", y="reorder frequency at 10 tests/week (years)", color="Conjugate")
 st.plotly_chart(fig, use_container_width=True)
 
-st.dataframe(data=res_p, use_container_width=True, 
-            column_config={"Source": st.column_config.LinkColumn(display_text="Source"),
-                            "supplier link": st.column_config.LinkColumn(display_text="Supplier Link")})
+# st.dataframe(data=res_p, use_container_width=True, 
+#             column_config={"Source": st.column_config.LinkColumn(display_text="Source"),
+#                             "supplier link": st.column_config.LinkColumn(display_text="Supplier Link")})
+
+@st.cache_data(show_spinner=False)
+def split_frame(input_df, rows):
+    df = [input_df.loc[i:i+rows-1,:] for i in range(0, len(input_df), rows)]
+    return df
+
+
+pagination = st.container()
+bottom_menu = st.columns((4, 1, 1))
+with bottom_menu[2]:
+    batch_size = st.selectbox("Page Size", options=[25, 50, 100])
+with bottom_menu[1]:
+    total_pages = (int(len(res_p.filter_df()) / batch_size) if int(len(res_p.filter_df()) / batch_size) > 0 else 1)
+    current_page = st.number_input("Page", min_value=1, max_value=total_pages, step=1)
+with bottom_menu[0]:
+    st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+
+pages = split_frame(res_p.filter_df(), batch_size)
+pagination.dataframe(data=pages[current_page - 1], use_container_width=True, 
+                     column_config={"Image": st.column_config.LinkColumn(display_text="Image here"),
+                     "Source": st.column_config.LinkColumn(display_text="Source"), 
+                     "supplier link": st.column_config.LinkColumn(display_text="Supplier Link")},
+                     height=900, column_order=columns_simple)
 
 add_auth(required=True)
 
