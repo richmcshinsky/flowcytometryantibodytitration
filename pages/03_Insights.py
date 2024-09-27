15adb8326a96fadcd686bdc53d1f315d25d29d26
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-09-27 16:43:50 +0000

    t

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -53,9 +53,8 @@ def normalize_antigens(df):
     return df
 
 
-columns_simple = ["Source", "supplier link", "Antigen", "Clone", "Conjugate", 
-                  "Supplier", "# of tests at optimal dilution", 
-             "price/test at optimal uL", "reorder frequency at 10 tests/week (years)"]
+columns_simple = ["Antigen", "Clone", "Conjugate", "Conjugate Type", "Test Tissue", "Test Cell Type",
+                  "Test Preparation", "Target Species"]
 
 df = load_data()
 df_filtered = DynamicFilters(df.astype(str).fillna(""), filters=columns_simple)
@@ -64,15 +63,12 @@ res = df_filtered.filter_df()
 
 st.write("Plot data from " + str(len(res["Source"].unique())) + " unique data sources.")
 
-# # of tests at optimal dilution       for antigens
-# price/test at optimal uL.            antigens
-# reorder frequency at 10 tests/week (years) 
-
 # fig = px.bar(res["Antigen"].value_counts(normalize=True)[:20])
 # st.plotly_chart(fig, use_container_width=True)
 
 st.write("Number of tests at optimal dilution comparison between suppliers")
-res_p = res[columns_simple].dropna().drop_duplicates()
+res_p = res[["Source", "supplier link", "Antigen", "Clone", "Conjugate", "Supplier", "# of tests at optimal dilution", 
+             "price/test at optimal uL", "reorder frequency at 10 tests/week (years)"]].dropna().drop_duplicates()
 res_p = res_p[res_p["price/test at optimal uL"] != "nan"]
 res_p = res_p[res_p["price/test at optimal uL"] != 0]
 fig = px.strip(res_p, x="Supplier", y="# of tests at optimal dilution", color="Antigen")
@@ -90,7 +86,7 @@ st.plotly_chart(fig, use_container_width=True)
 #             column_config={"Source": st.column_config.LinkColumn(display_text="Source"),
 #                             "supplier link": st.column_config.LinkColumn(display_text="Supplier Link")})
 
-# @st.cache_data(show_spinner=False)
+@st.cache_data(show_spinner=False)
 def split_frame(input_df, rows):
     df = [input_df.loc[i:i+rows-1,:] for i in range(0, len(input_df), rows)]
     return df
@@ -100,12 +96,12 @@ bottom_menu = st.columns((4, 1, 1))
 with bottom_menu[2]:
     batch_size = st.selectbox("Page Size", options=[25, 50, 100])
 with bottom_menu[1]:
-    total_pages = (int(len(df_filtered.filter_df()) / batch_size) if int(len(df_filtered.filter_df()) / batch_size) > 0 else 1)
+    total_pages = (int(len(res) / batch_size) if int(len(res) / batch_size) > 0 else 1)
     current_page = st.number_input("Page", min_value=1, max_value=total_pages, step=1)
 with bottom_menu[0]:
     st.markdown(f"Page **{current_page}** of **{total_pages}** ")
 
-pages = split_frame(df_filtered.filter_df(), batch_size)
+pages = split_frame(res, batch_size)
 pagination.dataframe(data=pages[current_page - 1], use_container_width=True, 
                      column_config={"Image": st.column_config.LinkColumn(display_text="Image here"),
                      "Source": st.column_config.LinkColumn(display_text="Source"), 
