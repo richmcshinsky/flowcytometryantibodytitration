08f3aa64f03616685e45f420c40365d0e72917a9
Richard McShinsky <47258478+richmcshinsky@users.noreply.github.com>
2024-09-29 15:50:51 +0000

    t

--- pages/05_Pricing.py
+++ pages/05_Pricing.py
@@ -14,11 +14,13 @@ with st.sidebar:
 st.markdown("<h1 style='text-align: center; color: black;'>Metrdy</h1>", unsafe_allow_html=True)
 st.divider()
 
-st.title("Repository benefits:")
-st.image("data/repo benifits.png")
+with st.columns(3)[1]:
+    st.title("Repository benefits")
+    st.image("data/repo benifits.png")
 
-st.title("Subscription benefits:")
-st.image("data/sub benifits.png")
+with st.columns(3)[1]:
+    st.title("Subscription benefits")
+    st.image("data/sub benifits.png")
 
 st.write("Pricing:")
 lst = ["$xx/month for individual access", 
