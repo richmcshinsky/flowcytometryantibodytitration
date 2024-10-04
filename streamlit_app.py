432d0c9f3608667d29a8a4f582096cab49b203c1
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-04 00:01:26 -0600

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,9 @@ with st.sidebar:
     # Search
     # Maybe cahnge purchase into tools?
 
-col1,col2 = st.columns(2)
-with col1:
-    st.markdown("<h1 style='text-align: center; color: black;'>Metrdy</h1> ", unsafe_allow_html=True)
-with col2:
-    st.html("""<a href="https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27"><img src="https://drive.google.com/file/d/1j4VZ_kqPD3r1DpsmHOiLVZyVF3zGRsEZ/view?usp=sharing" width="200" /></a>""")
-    # st.markdown("[![Click me](./app/static/soundicon.png)](https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27)")
+st.markdown("<h1 style='text-align: center; color: black;'>Metrdy</h1> ", unsafe_allow_html=True)
+# st.html("""<a href="https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27"><img src="https://drive.google.com/file/d/1j4VZ_kqPD3r1DpsmHOiLVZyVF3zGRsEZ/view?usp=sharing" width="200" /></a>""")
+# st.markdown("[![Click me](./app/static/soundicon.png)](https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27)")
 st.divider()
 
 st.subheader("Welcome")
