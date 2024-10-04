5a57c1c46f60e1b9ea386f63072cb9cedc7d7506
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-03 21:04:54 -0600

    t

--- pages/06_Guided.py
+++ pages/06_Guided.py
@@ -171,7 +171,7 @@ elif st.session_state["step"] == "Step 5":
     
     st.write("Number of tests at optimal dilution comparison between suppliers")
     res_p = df_g[["Source", "supplier link", "Antigen", "Supplier", "# of tests at optimal dilution", 
-                "price/test at optimal uL", "reorder frequency at 10 tests/week (years)"]].dropna().drop_duplicates()
+                "price/test at optimal uL", "reorder frequency at 10 tests/week (years)"]].dropna(subset=["price/test at optimal uL"]).drop_duplicates()
     res_p = res_p[res_p["price/test at optimal uL"] != "nan"]
     res_p = res_p[res_p["price/test at optimal uL"] != 0]
     fig = px.strip(res_p, x="Supplier", y="# of tests at optimal dilution")
@@ -185,7 +185,8 @@ elif st.session_state["step"] == "Step 5":
     fig = px.strip(res_p, x="Supplier", y="reorder frequency at 10 tests/week (years)")
     st.plotly_chart(fig, use_container_width=True)
 
-    st.dataframe(data=df_g, use_container_width=True, 
+    st.dataframe(data=df_g["Source", "supplier link", "Antigen", "Supplier", "# of tests at optimal dilution", 
+                "price/test at optimal uL", "reorder frequency at 10 tests/week (years)"], use_container_width=True, 
                 column_config={"Source": st.column_config.LinkColumn(display_text="Source"),
                                 "supplier link": st.column_config.LinkColumn(display_text="Supplier Link")})
     
