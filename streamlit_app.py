d678de6dc575be23f64fc18090c2576aecd9aae4
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-03 23:20:55 -0600

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from st_paywall import add_auth
 
 st.set_page_config(page_title='Flow Cytometry Antibody Titration Repository', layout="wide")
 
-st.markdown("[![](data/soundicon)](https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27)")
+st.markdown("[![""](data/soundicon)](https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27)")
 
 st.write("https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27")
 
@@ -27,7 +27,7 @@ with st.sidebar:
     # Search
     # Maybe cahnge purchase into tools?
 
-st.markdown("<h1 style='text-align: center; color: black;'>[![Metrdy](data/soundicon)](https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27)</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Metrdy</h1>", unsafe_allow_html=True)
 st.divider()
 
 st.subheader("Welcome")
