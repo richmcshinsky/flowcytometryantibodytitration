4c85bd1e2783932c011bdeb0b06f4fe3fe92e807
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-05 18:05:57 -0600

    t

--- pages/03_Insights.py
+++ pages/03_Insights.py
@@ -140,7 +140,7 @@ from bokeh.models import ColumnDataSource, OpenURL, TapTool
 from bokeh.plotting import figure
 p = figure(tools="tap", x_range=res_p["Supplier"].unique(), x_axis_label="Supplier", y_axis_label="# of tests at optimal dilution")
 source = ColumnDataSource(data=dict(x=res_p["Supplier"], y=res_p["# of tests at optimal dilution"], link=res_p["supplier link"]))
-p.scatter(x='x', y='y', size=10, source=source)
+p.circle(x='x', y='y', size=10, source=source)
 url = "@link"
 taptool = p.select(type=TapTool)
 taptool.callback = OpenURL(url=url)
