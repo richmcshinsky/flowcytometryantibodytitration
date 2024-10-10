9bab4b90ef012ceeb445aac56402c428d988a5db
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-09 22:55:38 -0600

    t

--- pages/05_Pricing.py
+++ pages/05_Pricing.py
@@ -25,6 +25,11 @@ with st.container():
              33 commercially purchased antibodies tested, 21 included in the final. 
              For the 21 in the final, summing the difference between supplier
              recommended and optimal dilution pricing per test, the total savings would be :blue[$54.61/test].""")
+    st.write("""We aimed to test 192 samples per experiment, which translates to approximately $42,000 of reagent costs 
+             (average cost of ∼$2.5/reagent/sample) for three 28-color panels. 
+             (https://www.cell.com/cell-reports-methods/fulltext/S2667-2375(23)00283-7?_returnURL=https%3A%2F%2Flinkinghub.elsevier.com%2Fretrieve%2Fpii%2FS2667237523002837%3Fshowall%3Dtrue). 
+             Although antibodies may seem cheap, they add up fast and can have a dramatic overall savings. Even a 20% reduction here would have 
+             reduced costs by :blue[$8,400] for the one paper.""")
     st.write("""For common cases, ordering appopriate amounts antibodies can draw from personal
              experience. But for uncommon situations, this can make a significant difference. For example,
              antigen CD56 with conjugate BUV737, at optimal dilution, the savings on the difference
@@ -36,6 +41,12 @@ with st.container():
              optimal dilution is $-8.6, demonstrating that ording the vial in what one would normally do
              would likely result in underording enough antigen, resulting in either :blue[delayed] experiments
              or :blue[poor quality] data.""")
+    st.write("""A major savings in cost comes from employee time spent in optimizing or having to redo an expirement because
+             and antibody failed due to a bad dilution. Traditionally, purchasing amounts have been done by a history kept at the lab,
+             what someone did before that worked, or hours and hours of research. Assuming a lab manager salary of 
+             $90,000/year or $45/hour, not including benefits, then for every 8 hours spent reading published papers, optimizing, 
+             reording, or other complications would correlate to :blue[$360/day] not counting material cost as well. 
+             Assuming that the employee knows where to look and knows what to do.""")
 
 with st.container():
     st.markdown("<h2 style='text-align: center; color: black;'>Subscription benefits</h2>", unsafe_allow_html=True)
