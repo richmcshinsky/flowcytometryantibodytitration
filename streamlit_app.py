c96c843e3e637fb00e711cae50c5878eeda63f31
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-20 01:38:12 -0600

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,11 @@ col1, col2, col3, col4 = st.columns([2,3,1,1], gap="small")
 with col2:
     st.markdown("""<h3 style='text-align: center; color: black;'>Cyto<u>metr</u>y Antibo<u>dy</u> Database</h3>""", unsafe_allow_html=True)
 with col3:
-    st.link_button("Pronunciation", "https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27")
-
-# st.html("""<a href="https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27"><img src="https://drive.google.com/file/d/1j4VZ_kqPD3r1DpsmHOiLVZyVF3zGRsEZ/view?usp=sharing" width="200" /></a>""")
-# st.markdown("[![Click me](./app/static/soundicon.png)](https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27)")
-
+    # st.link_button("Pronunciation", "https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27")
+    button_html = """<a href='https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27' target='_blank'>
+    <img src='data/sound.png' alt='Button Image' style='width:100px;height:50px;'></a>
+    """
+    st.markdown(button_html, unsafe_allow_html=True)
 
 st.divider()
 
