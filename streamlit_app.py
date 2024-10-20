40e8a5e9314df82c9c908c1664da6cbb2e6e81ca
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-20 01:25:28 -0600

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ st.markdown("<h3 style='text-align: center; color: black;'>Welcome</h3>", unsafe
 
 st.write("""Our aim is to democratize flow cytometry antibody data and give you the necessary tools to significantly 
          save your money and time. You will be able to look up curated data from publications and user contributions to 
-         easily make informed purchasing decisions between suppliers, with optimal concentrations from validated research. 
+         easily make informed purchasing decisions between suppliers; with optimal concentrations from validated research. 
          Another prime example of the potential use of our tools is the ability to seamlessly generate publication quality figures 
          from your own data. Anyone is welcome to contribute to expanding the Metrdy repository.""")
 
