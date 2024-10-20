401a2b415a8227a79ece51e972a099adbf9a18d5
richmcshinskyhci <richard.mcshinsky@hci.utah.edu>
2024-10-20 00:53:24 -0600

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,10 @@ with st.sidebar:
 
 
 st.markdown("<h1 style='text-align: center; color: black;'>Metrdy</h1>", unsafe_allow_html=True)
-col1, col2, col3 = st.columns([1,3,1], gap="small")
+col1, col2, col3, col4 = st.columns([1,2,1,1], gap="small")
 with col2:
     st.markdown("""<h3 style='text-align: center; color: black;'>Cyto<u>metr</u>y Antibo<u>dy</u> Database</h3>""", unsafe_allow_html=True)
+with col3:
     st.link_button("", "https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27", icon=":material/brand_awareness:")
 
 # st.html("""<a href="https://www.merriam-webster.com/dictionary/ma%C3%AEtre%20d%27"><img src="https://drive.google.com/file/d/1j4VZ_kqPD3r1DpsmHOiLVZyVF3zGRsEZ/view?usp=sharing" width="200" /></a>""")
